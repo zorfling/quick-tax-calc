f4e04cdd08d53d367d1b1084660ec589426d8d8d
Chris Colborne <zorfling@gmail.com>
2018-06-25 11:57:49 +1000

    Add analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+ReactGA.initialize('UA-1359868-17');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
